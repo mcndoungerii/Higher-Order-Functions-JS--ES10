26ba2f187e0129638fdff4fbccf3517388901204
Allen <allenmcndounger@gmail.com>
2019-12-01 18:41:07 +0300

    Assignment separate from declaration

--- destruct.js
+++ destruct.js
@@ -2,6 +2,13 @@
 const foo = ['one', 'two', 'three'];
 
 const [red,yellow,green] = foo;
-console.log(`Red value is ${red}`);
-console.log(`Yellow value is ${yellow}`);
-console.log(`Green value is ${green}`);
+// console.log(`Red value is ${red}`);
+// console.log(`Yellow value is ${yellow}`);
+// console.log(`Green value is ${green}`);
+
+//Array Destruct.. Assignment separate from declaration
+let a,b;
+
+[a,b] = [1,2];
+console.table(a);
+console.table(b);
