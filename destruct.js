fbc2c8c33b75dbddc8fb2feee54a5a95d247c1f0
Allen <allenmcndounger@gmail.com>
2019-12-01 18:56:28 +0300

    Swapping variables

--- destruct.js
+++ destruct.js
@@ -10,5 +10,27 @@ const [red,yellow,green] = foo;
 let a,b;
 
 [a,b] = [1,2];
-console.table(a);
-console.table(b);
+// console.table(a);
+// console.table(b);
+
+// Default values -- A variable can be assigned a default, in the case that the value unpacked from the array is undefined.
+let c,d;
+
+[c=1, d=7] = [1];
+
+// console.log(`The value of c is: ${c}`);
+// console.log(`The value of d is: ${d}`);
+
+
+//Swapping variables
+let e = 1;
+let f = 3;
+
+[e,f] = [f,e];
+
+// console.log(`The value of e is: ${e}`);
+// console.log(`The value of f is: ${f}`);
+
+const arr = [1,2,3,4,5];
+[arr[2], arr[1]] = [arr[1], arr[2]];
+//console.log(arr);  arr[2] = 3 === 2, arr[1] = 2 === 3; arr = [1,3,2]
