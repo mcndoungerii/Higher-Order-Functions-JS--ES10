976501ddc4f16ba2245121a4d442e4417ed90d6a
Allen <allenmcndounger@gmail.com>
2019-12-01 18:37:59 +0300

    Array Destructuring 1

---
+++ destruct.js
@@ -0,0 +1,7 @@
+//Array Destructuring
+const foo = ['one', 'two', 'three'];
+
+const [red,yellow,green] = foo;
+console.log(`Red value is ${red}`);
+console.log(`Yellow value is ${yellow}`);
+console.log(`Green value is ${green}`);
