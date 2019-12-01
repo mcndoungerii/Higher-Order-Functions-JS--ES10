8e13027851ea88985c241b1d2d487a93f7fca240
Allen <allenmcndounger@gmail.com>
2019-12-01 20:45:59 +0300

    Assigning to new variable names

--- objDestruct.js
+++ objDestruct.js
@@ -5,5 +5,18 @@
 const o = {p: 42, q: true};
 const {p,q} = o;
 
-console.log(p);
-console.log(q);
+// console.log(p);
+// console.log(q);
+
+//2.Assignment without declaration
+let a,b;
+({a,b} = {a: 1, b: 2}); // same as const {a,b} = {a:1, b:2}
+// console.log(a);
+// console.log(b);
+
+//3. Assigning to new variable names
+const o = {r: 42, s: true};
+const {r: foo, s: bar} = 0;
+
+console.log(r);
+console.log(s);
