7b100d8a2113e9ed893b238a92ff9d7ece2aae91
Allen <allenmcndounger@gmail.com>
2019-12-02 10:01:58 +0300

    Array.FlatMap()

--- es10.js
+++ es10.js
@@ -17,4 +17,11 @@ const cities = ['City 1', ['City 2'], ['City 3', 'City 4']];
 
 // it useful to mention that Array.flat will remove all empty slots from our array.
 const numbers = [1, 2, [3, 4],, 5, 6,, [7,8]];
-console.log(numbers.flat());
+// console.log(numbers.flat());
+
+//4. Array.flatMap() --> It returns array of objects with property names you want
+const userss = [['John', 49, 96], ['Frank', 25, 388], ['Davis',36, 14]];
+const usersFlattened = userss.flatMap(([name,age,followers]) => {
+    return {name,age,followers};
+})
+console.log(usersFlattened);
