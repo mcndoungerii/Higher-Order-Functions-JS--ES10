cefd8d825af67b4cfbc8fc38a82ce32f5ce0af26
Allen <allenmcndounger@gmail.com>
2019-12-05 11:24:18 +0300

    Array.flat

--- revise.js
+++ revise.js
@@ -11,5 +11,17 @@ Object.keys(res).forEach(key => {
     name: key,
     age: res[key]
   };
-  console.log(result);
+  //   console.log(result);
 });
+
+const people = [
+  ["Jon", 56, 900],
+  ["St Patricks", 41, 780],
+  ["Peace", 27, 760]
+];
+
+const resPeople = people.flatMap(([name, age, score]) => {
+  return { name, age, score };
+});
+
+console.log(resPeople);
