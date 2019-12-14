877557d9a3f8695c9f592a5ddf0386a48fd2b27b
Allen <allenmcndounger@gmail.com>
2019-12-14 15:13:15 +0300

    Update es7.js and revise.js

--- es7.js
+++ es7.js
@@ -1,16 +1,16 @@
-let arr = ['react', 'angular', 'vue'];
+let arr = ["react", "angular", "vue"];
 
-if(arr.includes('react')) {
-    // console.log('Can use React');
+if (arr.includes("react")) {
+  // console.log('Can use React');
 }
 
 //Instead of
-if(arr.indexOf('react') !== -1) {
-    // console.log('Can use React')
+if (arr.indexOf("react") !== -1) {
+  // console.log('Can use React')
 }
 //OR
-if(~arr.indexOf('react')) {
-    // console.log('can use react')
+if (~arr.indexOf("react")) {
+  // console.log('can use react')
 }
 
 //2.Exponential Operator ** is equivalent to Math.pow()
@@ -24,70 +24,70 @@ let b = 2 ** 4;
 //3. Object.values/ Object .entries
 
 //Before ES8 JavaScript developers who needed to iterate over own properties of an object had to use Object.keys, iterate over an array returned by it and use obj[key] to access each value
-let obj = {a:1, b:2, c:3};
+let obj = { a: 1, b: 2, c: 3 };
 let newList = [];
 Object.keys(obj).forEach(key => {
-    // console.log(`Key is ${key}, Value is ${obj[key]}`)
-    let newObj = {
-        number: obj[key]
-    }
-    newList.push(newObj);
+  // console.log(`Key is ${key}, Value is ${obj[key]}`)
+  let newObj = {
+    number: obj[key]
+  };
+  newList.push(newObj);
 });
 // console.log(newList);
 
-
 //ES6 -- use for of
-for(let key of Object.keys(obj)) {
-    // console.log(`Key is ${key}, Value is ${obj[key]}`)
+for (let key of Object.keys(obj)) {
+  // console.log(`Key is ${key}, Value is ${obj[key]}`)
 }
 
 //Object.values()
 
 let newArr = [];
 Object.values(obj).forEach(value => {
-    let newObj = {
-        value: value
-    }
-   newArr.push(newObj);
-})
+  let newObj = {
+    value: value
+  };
+  newArr.push(newObj);
+});
 // console.table(newArr);
 
 let newArrr = [];
-for(let val of Object.values(obj)) {
-    let newObj = {
-        values: val
-    }
-    newArrr.push(newObj);
+for (let val of Object.values(obj)) {
+  let newObj = {
+    values: val
+  };
+  newArrr.push(newObj);
 }
 // console.log(newArrr);
 
-//Object.flatMap()
-let objec = [['b',2],['h',13],['k',16]];
+//Array.flatMap()
+let objec = [
+  ["b", 2],
+  ["h", 13],
+  ["k", 16]
+];
 
-const newObjec = objec.flatMap(([letter,number]) => {
-    return {letter,number};
+const newObjec = objec.flatMap(([letter, number]) => {
+  return { letter, number };
 });
 // console.log(newObjec);
 
 //Using Object.entries(); -- forEach
 
-Object.entries(obj).forEach(([key,value]) => {
-    let newObjValue = {
-        id: value,
-        letter: key
-    };
+Object.entries(obj).forEach(([key, value]) => {
+  let newObjValue = {
+    id: value,
+    letter: key
+  };
 
-    // console.log(newObjValue);
-    
-
+  // console.log(newObjValue);
 });
 
 //Object.entries -- for od loop
-for(let [key,value] of Object.entries(obj)) {
-    let newObj = {
-        id: value,
-        letter: key
-    }
-    console.log(newObj);
+for (let [key, value] of Object.entries(obj)) {
+  let newObj = {
+    id: value,
+    letter: key
+  };
+  console.log(newObj);
 }
-
--- revise.js
+++ revise.js
@@ -72,5 +72,81 @@ Object.entries(obj).forEach(([key, value]) => {
     name: key,
     codeC: value
   };
-  console.log(newOb);
+  //   console.log(newOb);
 });
+
+//Object Destructuring;
+const o = { c: "James", d: 5 };
+const { c, d } = o;
+// console.log(c);
+// console.log(d);
+const { car = "Limosine", bike } = { bike: 6 };
+// console.log(car);
+
+let k, l;
+({ k = "Brown", l } = { l: "Brown" });
+// console.log(k);
+
+const resu = { p: "Tanzania", q: "Kenya", r: "Uganda", t: "Rwanda" };
+const { p: country, q, ...rest } = resu;
+// console.log(country);
+
+//Spread Operator
+
+var myName = { firstName: "Allen", lastName: "Mwandunga", age: 24 };
+var others = { ...myName, height: 20, weight: "70 Kg" };
+// console.log(others);
+
+//Object.fromEntries()
+
+const userss = [
+  ["Jon", 24],
+  ["kate", 62],
+  ["Mony", 37]
+];
+
+let newArr = [];
+const xy = Object.fromEntries(userss);
+// Object.keys(xy).forEach(key => {
+//   let newObj = {
+//     name: key,
+//     age: xy[key]
+//   };
+//   newArr.push(newObj);
+// });
+// console.log(newArr);
+for (let key of Object.keys(xy)) {
+  let newObj = {
+    name: key,
+    age: xy[key]
+  };
+  // console.log(newObj);
+}
+
+//Instead of using Object.fromEntries then Looping then storing keys & values in the new array.
+//The use of Array.flatMap
+
+const arrFlat = userss.flatMap(([name, age]) => {
+  return { name, age };
+});
+// console.log(arrFlat);
+
+const city = ["Dar", ["Vatican", "Pretoria"], ["Dodoma"], "Kigoma"];
+const test = city.flat();
+// console.log(test);
+
+const message = "   Hello ES10   ";
+// console.log(message.trimStart());
+
+// console.log(message.trimEnd());
+
+//The Use of Object.entries()
+Object.entries(xy).forEach(([key, value]) => {
+  let newObjj = {
+    name: key,
+    age: value
+  };
+  console.log(newObjj);
+});
+//This is good when you have rigid Object & you want to create a newObject from it
+//Is the same as using Object.keys()
